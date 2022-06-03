c469cadbfde6bf6ae9790c8b70772aa92f38ed65
DiLello <dilellocn@gmail.com>
2022-06-03 10:28:32 -0300

    Criado o bloco basico para abrir arquivos em loop

---
+++ abrir_arquivos.py
@@ -0,0 +1,18 @@
+import glob
+import pandas as pd
+
+#IMPORTANTE#
+#Criar uma pasta onde contenha APENAS os arquivos com dados brutos
+
+# ler todos os arquivos da pasta: 'Caminho/*.txt'
+
+CaminhoDadoBruto = 'Documents/Projetos/OrlaSemLixo/Linha1/*.txt'
+
+j = 0
+for i in glob.glob(CaminhoDadoBruto, recursive = True):
+    j += 1
+    globals()["dado" + str(j)] = pd.read_csv(i, sep = '\s+',header = None)
+
+
+
+
